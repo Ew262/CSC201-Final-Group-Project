030cf92ad00750f7ff9b3d80d3998e475d3423ff
Edward Ladia <egl21@email.vccs.edu>
2020-12-12 20:27:29 -0500

    Update PACE.cpp
    updated cpp file. final battleship shell program

--- PACE.cpp
+++ PACE.cpp
@@ -5,26 +5,26 @@ using namespace std;
 enum orientation {horizontal, vertical};
 
 struct PositionType {
-	int row;
-	int col;
+    int row;
+    int col;
 };
 
 struct ShipType {
-	char carrier[5]; // vertical
-	char battleship[4]; // horizontal
-	char cruiser[3]; // vertical
-	char submarine[3]; // horizontal
-	char destroyer[2]; // vertical
+    char carrier[5]; // vertical
+    char battleship[4]; // horizontal
+    char cruiser[3]; // vertical
+    char submarine[3]; // horizontal
+    char destroyer[2]; // vertical
 };
 
 const int MAX_SIZE = 8;
 
 struct GameType {
-	char gridArray[MAX_SIZE][MAX_SIZE];
+    char gridArray[MAX_SIZE][MAX_SIZE];
     int arrayGrid[MAX_SIZE];
-	int shipLocations[MAX_SIZE][MAX_SIZE];
-	ShipType ship;
-	PositionType position;
+    int shipLocations[MAX_SIZE][MAX_SIZE];
+    ShipType ship;
+    PositionType position;
 };
 
 // creates and returns a random number when called
@@ -39,47 +39,53 @@ void PrintRules();
 
 // Initializes battlefield
 // Pre: valid gridArray char
-//	valid shipLocations char
+//    valid shipLocations char
 // Post: returns char 2D array
-void InitBattlefield(/* INOUT */ char gridArray[][MAX_SIZE], int &arrayGrid[MAX_SIZE], int &rowLength);
+void InitBattlefield(/* INOUT */ char gridArray[][MAX_SIZE], int arrayGrid[MAX_SIZE], int &rowLength);
 
 // prints updated battleship to screen
 // Pre: valid const int array
-// Post: returns formatted 
+// Post: returns formatted
 void PrintBattlefield(/* IN */ const char array[][MAX_SIZE], int arrayGrid[MAX_SIZE], int rowLength);
 
 // prompts user for guess
 // Pre: valid x int
-//	valid y int
+//    valid y int
 // Post: prompts and returns user input
 void PromptGuesses(int &x, int &y);
 
-// interperets guesses 
+// interperets guesses
 // Pre: valid gridArray char
 //      valid shipLocations char
 //      valid x int
 //      valid y int
 // Post: returns wether a ship is hit missed
 //       and changes board with each input
-bool InterpretGuesses(/* INOUT */ const char gridArray[][MAX_SIZE], const int shipLocations[][MAX_SIZE], int x, int y);
+bool InterpretGuesses(int x, int y);
 
 // for making sure ships don't overlap
 // Pre:
-void CollisonCheck(const int shipLocations[][MAX_SIZE]);
+void CollisionCheck(const int shipLocations[][MAX_SIZE]);
  
 int main() {
-	char gameArray[MAX_SIZE][MAX_SIZE];
+    char arrayBoard[MAX_SIZE][MAX_SIZE];
     int arrayGrid[MAX_SIZE];
+    int shipLocations[MAX_SIZE][MAX_SIZE];
     int rowLength = 1; // set to one for ease of use for user
-	
-	//PrintRules();
-	//InitBattle();
-	
-	// while loop
-		//PrintBattleField();
-		//PromptGuesses();
-		//InterpretGuesses();
-	// while all ships are not hits and while user is still wants to play
+    int xGuess;
+    int yGuess;
+    
+    PrintRules();
+    InitBattlefield(arrayBoard, arrayGrid, rowLength);
+    
+    // while loop
+        PrintBattlefield(arrayBoard, arrayGrid, rowLength);
+        PromptGuesses(xGuess, yGuess);
+        InterpretGuesses (xGuess, yGuess);
+        CollisionCheck(shipLocations);
+    // while all ships are not hits and while user is still wants to play
+    
+    return 0;
 }
 
 int RandNumGen() {
@@ -92,7 +98,7 @@ void PrintRules() {
     cout << "PrintRules()\n";
 }
 
-void InitBattlefield(/* INOUT */ char gridArray[][MAX_SIZE], int &arrayGrid[MAX_SIZE], int &rowLength) {
+void InitBattlefield(/* INOUT */ char arrayBoard[][MAX_SIZE], int arrayGrid[MAX_SIZE], int &rowLength) {
     for (int row = 0; row < 8; row++) {
         arrayGrid[row] = rowLength++;
         for (int col = 0; col < 8; col++) {
@@ -101,7 +107,7 @@ void InitBattlefield(/* INOUT */ char gridArray[][MAX_SIZE], int &arrayGrid[MAX_
     }
 }
 
-void PrintBattlefield(/* IN */ const char array[][MAX_SIZE], int arrayGrid[MAX_SIZE], int rowLength) {       
+void PrintBattlefield(/* IN */ const char arrayBoard[][MAX_SIZE], int arrayGrid[MAX_SIZE], int rowLength) {
     cout << ' ';
     for (int row = 0; row < 8; row++) {
             cout << setw(6) << arrayGrid[row];
@@ -112,7 +118,7 @@ void PrintBattlefield(/* IN */ const char array[][MAX_SIZE], int arrayGrid[MAX_S
     for (int row = 0; row < 8; row++) {
         cout << arrayGrid[row];
         for (int col = 0; col < 8; col++) {
-            cout << setw(6) << arrBoard[row][col];
+            cout << setw(6) << arrayBoard[row][col];
         }
         cout << endl;
     }
@@ -122,11 +128,11 @@ void PromptGuesses(int &x, int &y) {
     cout << "PromptGuesses\n";
 }
 
-bool InterpretGuesses(/* INOUT */ const char gridArray[][MAX_SIZE], const int shipLocations[][MAX_SIZE],  
-                                  int x, int y) {
+bool InterpretGuesses(int x, int y) {
     cout << "InterpretGuesses\n";
+    return true;
 }
 
-void CollisonCheck(const char shipLocations[][MAX_SIZE]) {
+void CollisionCheck(const int shipLocations[][MAX_SIZE]) {
     cout << "CollisonCheck\n";
 }
